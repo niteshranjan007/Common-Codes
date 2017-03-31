f72712882a1f5ca51faa01b5b0dca2ca15cf05dc
niteshranjan007 <niteshranjan1992@gmail.com>
2017-03-31 14:47:26 +0530

    Add files via upload

---
+++ Excel.java
@@ -0,0 +1,52 @@
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+
+
+public class Excel {
+
+	
+    public static void main(String[] args) {
+	try {
+	    Class.forName("driverName");
+	    Connection con = DriverManager.getConnection("url", "user", "pass");
+	    Statement st = con.createStatement();
+	    ResultSet rs = st.executeQuery("Select * from tablename");
+	    HSSFWorkbook workbook = new HSSFWorkbook();
+	    HSSFSheet sheet = workbook.createSheet("lawix10");
+	    HSSFRow rowhead = sheet.createRow((short) 0);
+	    rowhead.createCell((short) 0).setCellValue("CellHeadName1");
+	    rowhead.createCell((short) 1).setCellValue("CellHeadName2");
+	    rowhead.createCell((short) 2).setCellValue("CellHeadName3");
+	    int i = 1;
+	    while (rs.next()){
+	        HSSFRow row = sheet.createRow((short) i);
+	        row.createCell((short) 0).setCellValue(Integer.toString(rs.getInt("column1")));
+	        row.createCell((short) 1).setCellValue(rs.getString("column2"));
+	        row.createCell((short) 2).setCellValue(rs.getString("column3"));
+	        i++;
+	    }
+	    String yemi = "g:/test.xls";
+	    FileOutputStream fileOut = new FileOutputStream(yemi);
+	    workbook.write(fileOut);
+	    fileOut.close();
+	    } catch (ClassNotFoundException e1) {
+	       e1.printStackTrace();
+	    } catch (SQLException e1) {
+	        e1.printStackTrace();
+	    } catch (FileNotFoundException e1) {
+	        e1.printStackTrace();
+	    } catch (IOException e1) {
+	        e1.printStackTrace();
+	    }
+    }
+}
